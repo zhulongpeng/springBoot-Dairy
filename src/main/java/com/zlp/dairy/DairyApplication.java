e0486b052c24417f673bfa8ebe0625cdaa1436ad
Ygritte <ygritte.zhu>
2019-01-04 10:36:55 +0800

    fix

--- src/main/java/com/zlp/dairy/DairyApplication.java
+++ src/main/java/com/zlp/dairy/DairyApplication.java
@@ -1,11 +1,7 @@
 package com.zlp.dairy;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class DairyApplication {
