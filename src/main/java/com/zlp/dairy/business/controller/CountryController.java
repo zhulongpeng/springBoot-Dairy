de5f9307538e537a60f8785a3717099fa7857d53
Ygritte <ygritte.zhu>
2019-02-13 10:14:16 +0800

    文件操作

--- src/main/java/com/zlp/dairy/business/controller/CountryController.java
+++ src/main/java/com/zlp/dairy/business/controller/CountryController.java
@@ -1,11 +1,13 @@
 package com.zlp.dairy.business.controller;
 
 
+import com.zlp.dairy.base.controller.BaseController;
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(value = "country")
 @RestController
-public class CountryController {
+public class CountryController extends BaseController {
+
 
 }
