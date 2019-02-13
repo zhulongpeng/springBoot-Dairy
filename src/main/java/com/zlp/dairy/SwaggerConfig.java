66231bc050be2a029a1a9ffd25fb7b5a1ce2a6a1
Ygritte <ygritte.zhu>
2019-02-13 15:10:52 +0800

    文件操作

--- src/main/java/com/zlp/dairy/SwaggerConfig.java
+++ src/main/java/com/zlp/dairy/SwaggerConfig.java
@@ -21,7 +21,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.zlp.dairy"))
+                .apis(RequestHandlerSelectors.basePackage("com.zlp.dairy.business.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
