00d7cdb843baf1421fdb58038df9a105a2f2aae9
Ygritte <ygritte.zhu>
2019-02-13 17:02:11 +0800

    文件操作

--- src/main/java/com/zlp/dairy/WebMvcConfigurer.java
+++ src/main/java/com/zlp/dairy/WebMvcConfigurer.java
@@ -16,7 +16,8 @@ import java.util.List;
 @EnableWebMvc
 public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
 
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
@@ -24,6 +25,7 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
         super.addResourceHandlers(registry);
     }
+
 
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
