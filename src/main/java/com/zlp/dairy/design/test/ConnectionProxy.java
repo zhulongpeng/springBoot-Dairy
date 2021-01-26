481702139c790dc023941400c195dc63151121cd
Ygritte Zhu <ygritte.zhu@3ti.us>
2021-01-26 16:09:29 +0800

    javaSE

---
+++ src/main/java/com/zlp/dairy/design/test/ConnectionProxy.java
@@ -0,0 +1,9 @@
+package com.zlp.dairy.design.test;
+
+/**
+ * Created by Ygritte Zhu on 2021/1/26
+ * 代理模式
+ */
+public class ConnectionProxy {
+
+}
--- src/main/java/com/zlp/dairy/design/test/Singleton.java
+++ src/main/java/com/zlp/dairy/design/test/Singleton.java
@@ -6,12 +6,20 @@ package com.zlp.dairy.design.test;
  */
 public class Singleton {
 
+    /**
+     * 分配内存
+     * 初始化函数
+     * 将对象指向分配内存的地址
+     */
+
     private Singleton(){
 
     }
 
+    //私有化构造函数
     private static Singleton singleton = new Singleton();
 
+    //公共的静态方法返回一个单一实例
     public synchronized static Singleton getInstance(){
         if(singleton == null){
             synchronized (Singleton.class) {
