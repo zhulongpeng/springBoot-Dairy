fa0afde43141f4e26c43eaa132e5ae62889e4a06
Ygritte Zhu <ygritte.zhu@3ti.us>
2021-02-01 15:50:55 +0800

    javaSE

--- src/test/java/com/zlp/business/DemoTest.java
+++ src/test/java/com/zlp/business/DemoTest.java
@@ -7,7 +7,6 @@ import com.zlp.dairy.business.entity.Item;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
